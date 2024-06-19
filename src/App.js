8f5d8d6c24b2e0949106381787935c8099765f70
Vishal Gaur <vishalgaur2002@gmail.com>
2024-06-19 19:45:53 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,65 +1,54 @@
 import './App.css';
-import Navbar from './conponents/Navbar';
-import Textform from './conponents/Textform';
-// import About from './conponents/About';
-import Alert from './conponents/Alert';
-import React, { useState } from 'react'
-
-
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import About from './components/About';
+import Alert from './components/Alert';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('light');
-  const [alert, setAlert] = useState(null); 
+  const [alert, setAlert] = useState(null);
 
-  
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-        setAlert(null);
-      }, 1500);
-  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
   const toggleMode = (gg) => {
-    // if (mode === 'light') {
-    //   setMode('dark');
-    //   document.body.style.backgroundColor = '#52529d'
-    //   showAlert("Dark mode has been enabled", "success");
-    // }
-    // else if (mode=='dark') { 
-    //   setMode('light');
-    //   document.body.style.backgroundColor = 'white'
-    //   showAlert("Light mode has been enabled", "success");
-    // }
-    if(gg){
+    if (gg) {
       setMode('light');
-      document.body.style.backgroundColor = 'white'
+      document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-    }
-    else {
+    } else {
       setMode('dark');
-      document.body.style.backgroundColor = '#52529d'
+      document.body.style.backgroundColor = '#52529d';
       showAlert("Dark mode has been enabled", "success");
     }
-  }
+  };
 
-
-  const ccr = (color) =>{
-    console.log('buttonClicked')
+  const ccr = (color) => {
+    console.log('buttonClicked');
     setMode(color);
-  }
+  };
 
   return (
     <>
-      <Navbar title="TextUtils" About="About" mode={mode} toggleMode={toggleMode} colorchange={ccr}/>
-        <Alert alert={alert}/>
-      <div className="container my-3">
-        <Textform showAlert={showAlert} heading="Enter Text to Analyze Below" mode={mode} />
-        {/* <About/> */}
-      </div>
-
+      <Router>
+        <Navbar title="TextUtils" About="About" mode={mode} toggleMode={toggleMode} colorchange={ccr} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<Textform showAlert={showAlert} heading="Enter Text to Analyze Below" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 // import PropTypes from 'prop-types'
 import './custom-styles.css';
+import { Link } from 'react-router-dom';
 
 
 
@@ -8,26 +9,32 @@ import './custom-styles.css';
 
 
 export default function Navbar(props) {
+
+
   const [isToggled, setIsToggled] = useState(true)
   const toggleHandler=()=>{
     setIsToggled(!isToggled);
     props.toggleMode(!isToggled);
   }
+
+
   console.log(props.mode);
+
+  
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">{props.title}</a>
+        <Link className="navbar-brand" to="/">{props.title}</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">{props.About}</a>
+              <Link className="nav-link" to="/about">{props.About}</Link>
             </li>
 
           </ul>
